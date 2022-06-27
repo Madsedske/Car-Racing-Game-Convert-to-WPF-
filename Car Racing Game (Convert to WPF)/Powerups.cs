ce3f548f209bc00a83605230e10447c48768ee08
Madsedske <88845582+Madsedske@users.noreply.github.com>
2022-06-27 10:41:26 +0200

    Add project files.

---
+++ Car Racing Game (Convert to WPF)/Powerups.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Racing_Game__Convert_to_WPF_
+{
+    public class Powerups
+    {
+
+    }
+}
